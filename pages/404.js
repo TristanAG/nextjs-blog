5752f5be54d2f6c1ea4aacd65b54873882c50904
Tristan Gruener <tristan.gruener@gmail.com>
2020-08-28 17:16:48 -0700

    updated 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 import Layout, { siteTitle } from '../components/layout'
 
 export default function Custom404() {
-  return <Layout><h1>404 - Page Not Found</h1></Layout>
+  return <Layout><h1>404 - Page Not Found 🤷‍♂️ sorry </h1></Layout>
 }
